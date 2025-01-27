32145cecfe1976f949e8321dbe3cdab6c9b2be12
Ryan Williams <ryan.williams@intersectmbo.org>
2025-01-27 15:45:02 +0000

    feat: add transaction id to successful submission

--- scripts/drep/delegate-to-self.sh
+++ scripts/drep/delegate-to-self.sh
@@ -3,6 +3,10 @@
 # Define directories
 keys_dir="./keys"
 txs_dir="./txs/drep"
+tx_path_stub="$txs_dir/vote-deleg-tx"
+tx_cert_path="$tx_path_stub.cert"
+tx_unsigned_path="$tx_path_stub.unsigned"  
+tx_signed_path="$tx_path_stub.signed"
 
 # Get the script's directory
 script_dir=$(dirname "$0")
@@ -27,26 +31,36 @@ echo "Delegating your voting rights to your DRep ID."
 container_cli conway stake-address vote-delegation-certificate \
  --stake-verification-key-file $keys_dir/stake.vkey \
  --drep-key-hash "$(cat $keys_dir/drep.id)" \
- --out-file $txs_dir/vote-deleg-key-hash.cert
+ --out-file $tx_cert_path
 
 echo "Building transaction"
 
 container_cli conway transaction build \
  --witness-override 2 \
- --tx-in $(container_cli conway query utxo --address $(cat $keys_dir/payment.addr) --out-file  /dev/stdout | jq -r 'keys[0]') \
+ --tx-in $(container_cli conway query utxo --address $(cat $keys_dir/payment.addr) --out-file  /dev/stdout | jq -r 'keys[20]') \
  --change-address $(cat $keys_dir/payment.addr) \
- --certificate-file $txs_dir/vote-deleg-key-hash.cert \
- --out-file $txs_dir/vote-deleg-tx.unsigned
+ --certificate-file $tx_cert_path \
+ --out-file $tx_unsigned_path
 
 echo "Signing transaction"
 
 container_cli conway transaction sign \
- --tx-body-file $txs_dir/vote-deleg-tx.unsigned \
+ --tx-body-file $tx_unsigned_path \
  --signing-key-file $keys_dir/payment.skey \
  --signing-key-file $keys_dir/stake.skey \
- --out-file $txs_dir/vote-deleg-tx.signed
+ --out-file $tx_signed_path
 
 echo "Submitting transaction"
 
-container_cli conway transaction submit \
- --tx-file $txs_dir/vote-deleg-tx.signed
+# Submit the transaction and capture the output
+transaction_output=$(container_cli conway transaction submit --tx-file $tx_signed_path)
+
+# Check if the transaction was successfully submitted
+if [[ $transaction_output == *"Transaction successfully submitted."* ]]; then
+  # Get the transaction ID
+  transaction_id=$(container_cli conway transaction txid --tx-file $tx_signed_path)
+  echo "Transaction successfully submitted. Follow the transaction at: $transaction_id"
+else
+  echo "Transaction submission failed, see submit error:"
+  echo "$transaction_output"
+fi
