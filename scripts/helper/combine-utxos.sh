08a9a482d3c4061a619865719b9d33d78a467b89
Ryan Williams <ryan.williams@intersectmbo.org>
2025-05-14 22:35:08 +0100

    tidy up

--- scripts/helper/combine-utxos.sh
+++ scripts/helper/combine-utxos.sh
@@ -2,7 +2,7 @@
 
 # Define directories
 keys_dir="./keys"
-txs_dir="./txs/combine-utxos"
+txs_dir="./txs/"
 
 # Get the script's directory
 script_dir=$(dirname "$0")
@@ -22,23 +22,24 @@ container_cli() {
   docker exec -ti $container_name cardano-cli "$@"
 }
 
-# build command
+echo "Combining all only ada UTxOs at $(cat "$keys_dir/payment.addr")"
+
+# build transaction
 container_cli conway transaction build \
-  --witness-override 1 \
     $(container_cli query utxo --address $(cat "$keys_dir/payment.addr") --out-file /dev/stdout \
         | jq -r 'to_entries 
         | map(select(.value.datum == null and .value.datumhash == null and .value.inlineDatum == null and .value.inlineDatumRaw == null and .value.referenceScript == null)) 
         | map(" --tx-in " + .key) 
         | .[]') \
   --change-address $(cat "$keys_dir/payment.addr") \
-  --out-file "$txs_dir/combine-tx.unsigned"
+  --out-file "$txs_dir/combine-utxos-tx.unsigned"
 
 # Sign transaction
 container_cli conway transaction sign \
-  --tx-body-file "$txs_dir/combine-tx.unsigned" \
+  --tx-body-file "$txs_dir/combine-utxos-tx.unsigned" \
   --signing-key-file "$keys_dir/payment.skey" \
-  --out-file "$txs_dir/combine-tx.signed"
+  --out-file "$txs_dir/combine-utxos-tx.signed"
 
 # Submit transaction
 container_cli conway transaction submit \
-  --tx-file "$txs_dir/combine-tx.signed"
+  --tx-file "$txs_dir/combine-utxos-tx.signed"
