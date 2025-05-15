82de1cb8945d4333f359e4d3423389f37fa8a19c
Ryan Williams <ryan.williams@intersectmbo.org>
2025-05-15 11:13:33 +0100

    redo cc scripts

--- scripts/cc/authorize-hot-key.sh
+++ scripts/cc/authorize-hot-key.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Define directories
+# Define directory paths
 keys_dir="./keys"
 txs_dir="./txs/cc"
+tx_path_stub="$txs_dir/auth-hot"
+tx_cert_path="$tx_path_stub.cert"
+tx_unsigned_path="$tx_path_stub.unsigned"
+tx_signed_path="$tx_path_stub.signed"
 
 # Get the script's directory
 script_dir=$(dirname "$0")
@@ -27,7 +31,7 @@ echo "Authorizing hot key for the constitutional committee."
 container_cli conway governance committee create-hot-key-authorization-certificate \
   --cold-verification-key-file "$keys_dir/cc-cold.vkey" \
   --hot-key-file "$keys_dir/cc-hot.vkey" \
-  --out-file "$txs_dir/auth-hot.cert"
+  --out-file "$tx_cert_path"
 
 echo "Building transaction"
 
@@ -35,18 +39,25 @@ container_cli conway transaction build \
   --witness-override 2 \
   --tx-in $(container_cli conway query utxo --address $(cat "$keys_dir/payment.addr") --out-file /dev/stdout | jq -r 'keys[0]') \
   --change-address $(cat "$keys_dir/payment.addr") \
-  --certificate-file "$txs_dir/auth-hot.cert" \
-  --out-file "$txs_dir/auth-hot-tx.unsigned"
+  --certificate-file "$tx_cert_path" \
+  --out-file "$tx_unsigned_path"
 
 echo "Signing transaction"
 
 container_cli conway transaction sign \
-  --tx-body-file "$txs_dir/auth-hot-tx.unsigned" \
+  --tx-body-file "$tx_unsigned_path" \
   --signing-key-file "$keys_dir/payment.skey" \
   --signing-key-file "$keys_dir/cc-cold.skey" \
-  --out-file "$txs_dir/auth-hot-tx.signed"
+  --out-file "$tx_signed_path"
 
+# Submit the transaction
 echo "Submitting transaction"
 
-container_cli conway transaction submit \
-  --tx-file "$txs_dir/auth-hot-tx.signed"
+if container_cli conway transaction submit --tx-file $tx_signed_path; then
+  # Get the transaction ID
+  transaction_id=$(container_cli conway transaction txid --tx-file $tx_signed_path)
+  echo "Follow the transaction at: $transaction_id"
+else
+  echo "Transaction submission failed."
+  exit 1
+fi
