6b56e0a1f9577aca5b2d111b0205724ca8f2d20e
Ryan Williams <ryan.williams@intersectmbo.org>
2025-05-16 16:31:11 +0100

    add send ada script

---
+++ scripts/helper/send-ada.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# ~~~~~~~~~~~~ CHANGE THIS ~~~~~~~~~~~~
+LOVELACE_AMOUNT=10000000
+ADDRESS="addr1q8w3x5v4g5k6h7j8k9l0m1n2o3p4q5r6s7t8u9v0w1x2y3z4a5b6c7d8e9f0g1h2i3j4k5l6m7n8o9p0q1r2s3t4u5v6w7x8y9z0"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Define directory paths
+keys_dir="./keys"
+txs_dir="./txs"
+tx_path_stub="$txs_dir/send-ada"
+tx_unsigned_path="$tx_path_stub.unsigned"
+tx_signed_path="$tx_path_stub.signed"
+
+# Get the script's directory
+script_dir=$(dirname "$0")
+
+# Get the container name from the get-container script
+container_name="$("$script_dir/../helper/get-container.sh")"
+
+if [ -z "$container_name" ]; then
+  echo "Failed to determine a running container."
+  exit 1
+fi
+
+echo "Using running container: $container_name"
+
+# Function to execute cardano-cli commands inside the container
+container_cli() {
+  docker exec -ti $container_name cardano-cli "$@"
+}
+
+# Send ada to the multisig payment script
+echo "Sending $LOVELACE_AMOUNT lovelace to the multisig payment address."
+
+echo "Building transaction"
+
+container_cli conway transaction build \
+ --tx-in $(container_cli conway query utxo --address $(cat $keys_dir/payment.addr) --out-file  /dev/stdout | jq -r 'keys[0]') \
+ --tx-out "$ADDRESS"+$LOVELACE_AMOUNT \
+ --change-address $(cat $keys_dir/payment.addr) \
+ --out-file "$tx_unsigned_path"
+
+container_cli conway transaction sign \
+  --tx-body-file "$tx_unsigned_path" \
+  --signing-key-file $keys_dir/payment.skey \
+  --out-file "$tx_signed_path"
+
+# Submit the transaction
+echo "Submitting transaction"
+
+if container_cli conway transaction submit --tx-file $tx_signed_path; then
+  # Get the transaction ID
+  transaction_id=$(container_cli conway transaction txid --tx-file $tx_signed_path)
+  echo "Follow the transaction at: $transaction_id"
+else
+  echo "Transaction submission failed."
+  exit 1
+fi
