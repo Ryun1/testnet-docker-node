6e0ddf695cccca920b887d2055cbe5903112fbf3
Ryan Williams <ryan.williams@intersectmbo.org>
2025-10-23 23:02:38 +0100

    add script to withdraw staking rewards

---
+++ scripts/stake/withdraw-rewards.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Define directory paths
+keys_dir="./keys"
+txs_dir="./txs/stake"
+tx_path_stub="$txs_dir/stake-withdraw"
+tx_cert_path="$tx_path_stub.cert"
+tx_unsigned_path="$tx_path_stub.unsigned"
+tx_signed_path="$tx_path_stub.signed"
+
+# Get the script's directory
+script_dir=$(dirname "$0")
+
+# Get the container name from the get-container script
+container_name="$("$script_dir/../helper/get-container.sh")"
+
+if [ -z "$container_name" ]; then
+  echo "Failed to determine a running container."
+  exit 1
+fi
+
+echo "Using running container: $container_name"
+
+# Function to execute cardano-cli commands inside the container
+container_cli() {
+  docker exec -ti $container_name cardano-cli "$@"
+}
+
+stake_account_balance=$(container_cli conway query stake-address-info --address $(cat $keys_dir/stake.addr) | jq -r '.[0].rewardAccountBalance') 
+
+# Send ada to the multisig payment script
+echo "Withdrawing all balance of $stake_account_balance from stake account."
+
+echo "Building transaction"
+
+container_cli conway transaction build \
+ --tx-in $(container_cli conway query utxo --address $(cat $keys_dir/payment.addr) --out-file  /dev/stdout | jq -r 'keys[0]') \
+ --change-address $(cat $keys_dir/payment.addr) \
+ --withdrawal $(cat $keys_dir/stake.addr)+$stake_account_balance \
+ --out-file "$tx_unsigned_path"
+
+echo "Signing transaction"
+
+container_cli conway transaction sign \
+  --tx-body-file "$tx_unsigned_path" \
+  --signing-key-file $keys_dir/payment.skey \
+  --signing-key-file $keys_dir/stake.skey \
+  --out-file "$tx_signed_path"
+
+# Submit the transaction
+echo "Submitting transaction"
+
+container_cli conway transaction submit --tx-file $tx_signed_path
