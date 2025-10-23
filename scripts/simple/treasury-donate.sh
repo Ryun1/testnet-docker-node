f0a1b5432abfa07567b410b656957e2c5f0dd088
Ryan Williams <ryan.williams@intersectmbo.org>
2025-10-23 22:54:08 +0100

    add treasury donation script

---
+++ scripts/simple/treasury-donate.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# ~~~~~~~~~~~~ CHANGE THIS ~~~~~~~~~~~~
+LOVELACE_AMOUNT=10000000
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Define directory paths
+keys_dir="./keys"
+txs_dir="./txs"
+tx_path_stub="$txs_dir/treasury-donate"
+tx_unsigned_path="$tx_path_stub.unsigned"
+tx_signed_path="$tx_path_stub.signed"
+
+# Get the script's directory
+script_dir=$(dirname "$0")
+
+# Get the container name from the get-container script
+container_name="$("$script_dir/../helper/get-container.sh")"
+
+if [ -z "$container_name" ]; then
+  echo "Failed to determine a running container."
+  exit 1
+fi
+
+echo "Using running container: $container_name"
+
+# Function to execute cardano-cli commands inside the container
+container_cli() {
+  docker exec -ti $container_name cardano-cli "$@"
+}
+
+# Send ada to the multisig payment script
+echo "Sending $LOVELACE_AMOUNT lovelace to the treasury."
+
+echo "Building transaction"
+
+container_cli conway transaction build \
+ --tx-in $(container_cli conway query utxo --address $(cat $keys_dir/payment.addr) --out-file  /dev/stdout | jq -r 'keys[0]') \
+ --change-address $(cat $keys_dir/payment.addr) \
+ --treasury-donation $LOVELACE_AMOUNT \
+ --out-file "$tx_unsigned_path"
+
+container_cli conway transaction sign \
+  --tx-body-file "$tx_unsigned_path" \
+  --signing-key-file $keys_dir/payment.skey \
+  --out-file "$tx_signed_path"
+
+# Submit the transaction
+echo "Submitting transaction"
+
+container_cli conway transaction submit --tx-file $tx_signed_path
